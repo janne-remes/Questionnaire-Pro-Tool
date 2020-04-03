caeebc96f1ddd6edbebce8cc77b05584ce7e8373
janne-remes <janneremes@outlook.com>
2020-04-03 09:42:10 +0300

    Add project files.

---
+++ Questionnaire-project/DataHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questionnaire_project
+{
+    class DataHandler
+    {
+    }
+}
---
+++ Questionnaire-project/Question.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Questionnaire_project
+{
+    class Question
+    {
+        private string wholeQuestion;
+        private bool answer;
+
+        public Question()
+        {
+
+        }
+
+        public Question(string wholeQuestion, bool answer)
+        {
+            this.wholeQuestion = wholeQuestion;
+            this.answer = answer;
+        }
+
+        public string WholeQuestion
+        {
+            get
+            {
+                return wholeQuestion;
+            }
+
+            set
+            {
+                if (value != "")
+                {
+                    wholeQuestion = value;
+                }
+            }
+        }
+
+        public bool Answer
+        {
+            get
+            {
+                return answer;
+            }
+
+            set
+            {
+                answer = value;
+            }
+        }
+    }
+}
